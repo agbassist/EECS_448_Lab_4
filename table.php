a55ceed3a9cf1e8d1a15b099d79659df6d7584e8
Alex Griffin <agbassist@gmail.com>
2016-09-28 09:43:09 -0500

    Fixed it.

--- table.php
+++ table.php
@@ -3,12 +3,12 @@
    </head>
 <body>
 
-    <table style="width:100%">
+    <table>
         <?php
             for($x=1;$x <= 100;$x++){
                 echo "<tr>";
                 for($y=1;$y <= 100;$y++){
-                    echo "<th>" + $x*$y + "</th>";
+                    echo "<th>" . $x*$y . "</th>";
                 }
                 echo "</tr>";
             }
